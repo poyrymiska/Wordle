459bfb295368712b101b56b5aa96d27a568fd794
Miska Poyry <bhd692@myy.haaga-helia.fi>
2025-01-08 17:25:42 +0200

    Finish instructions

--- src/components/Instructions.tsx
+++ src/components/Instructions.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Button, Modal, ModalBody, ModalHeader } from "reactstrap";
+import { Button, Col, Modal, ModalBody, ModalHeader, Row } from "reactstrap";
 
 const Instructions = () => {
 
@@ -9,19 +9,72 @@ const Instructions = () => {
 
     return (
         <>
-            <Button onClick={toggleInstructions} color="secondary" size="lg" style={{ width: "100%", borderRadius: "10px", padding: "10px 20px", fontSize: "1.2rem"}}>
+            <Button onClick={toggleInstructions} color="secondary" size="lg" style={{ width: "100%", borderRadius: "10px", padding: "10px 20px", fontSize: "1.2rem" }}>
                 INSTRUCTIONS
             </Button>
-            <Modal isOpen={instructions} toggle={toggleInstructions}>
-                <ModalHeader toggle={toggleInstructions}>
-                    How to play
-                </ModalHeader>
+            <Modal isOpen={instructions} toggle={toggleInstructions} size="">
+                <ModalHeader toggle={toggleInstructions}>How to play</ModalHeader>
                 <ModalBody>
-                    <p>
-                        Add instructions, text and pictures here
-                    </p>
+                    <Col className="text-center">
+                        <h3>PYRI ARVAAMAAN SANA KUUDEN YRITYKSEN SISÄLLÄ</h3>
+                        <p style={{ fontSize: "1.2rem", marginTop: "10px", textTransform: "uppercase", lineHeight:"18px"}}>
+                            Tavoitteena on arvata piilotettu viiden kirjaimen sana mahdollisimman vähillä yrityksillä. Jokaisen arvauksen jälkeen saat vihjeitä:
+                        </p>
+                    </Col>
+                    <Row
+                        className="align-items-center mb-4 justify-content-center mx-auto"
+                        style={{
+                            border: "2px solid black",
+                            backgroundColor: "#f8f9fa",
+                            padding: "15px",
+                            borderRadius: "10px",
+                            boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
+                            height: "125px",
+                        }}
+                    >
+                        <Col className="text-center">
+                            <p style={{ fontSize: "1.2rem", margin: "0", color: "#6c757d" }}>
+                                <strong>HARMAA</strong> - VÄÄRÄ KIRJAIN VÄÄRÄSSÄ PAIKASSA
+                            </p>
+                        </Col>
+                    </Row>
+                    <Row
+                        className="align-items-center mb-4 justify-content-center mx-auto"
+                        style={{
+                            border: "2px solid black",
+                            backgroundColor: "#fffbea",
+                            padding: "15px",
+                            borderRadius: "10px",
+                            boxShadow: "0 10px 10px rgba(0, 0, 0, 0.1)",
+                            height: "125px"
+                        }}
+                    >
+                        <Col className="text-center">
+                            <p style={{ fontSize: "1.2rem", margin: "0", color: "#b89b00" }}>
+                                <strong>KELTAINEN</strong> - OIKEA KIRJAIN VÄÄRÄSSÄ PAIKASSA
+                            </p>
+                        </Col>
+                    </Row>
+                    <Row
+                        className="align-items-center justify-content-center mx-auto"
+                        style={{
+                            border: "2px solid black",
+                            backgroundColor: "#e6f4ea",
+                            padding: "15px",
+                            borderRadius: "10px",
+                            boxShadow: "0 10px 10px rgba(0, 0, 0, 0.1)",
+                            height: "125px"
+                        }}
+                    >
+                        <Col className="text-center">
+                            <p style={{ fontSize: "1.2rem", margin: "0", color: "#2c6e49" }}>
+                                <strong>VIHREÄ</strong> - OIKEA KIRJAIN OIKEASSA PAIKASSA
+                            </p>
+                        </Col>
+                    </Row>
                 </ModalBody>
             </Modal>
+
         </>
     )
 
